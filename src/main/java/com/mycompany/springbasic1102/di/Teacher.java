d8ab8174d281ee3988d0b982f9d67611548a4235
student <student@192.168.1.92>
2020-11-06 20:39:53 +0800

    SSH

--- src/main/java/com/mycompany/springbasic1102/di/Teacher.java
+++ src/main/java/com/mycompany/springbasic1102/di/Teacher.java
@@ -1,12 +1,23 @@
 package com.mycompany.springbasic1102.di;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Teacher {
     private String name;
     private List<String> cls; //班級
-    private Set<Student> students;
+    private Set<Student> students; //教導的學生
+    private Map<String,Integer> subject;//教導的科目跟時數
+
+    public Map<String, Integer> getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Map<String, Integer> subject) {
+        this.subject = subject;
+    }
+    
 
     public Set<Student> getStudents() {
         return students;
@@ -41,8 +52,10 @@ public class Teacher {
 
     @Override
     public String toString() {
-        return "Teacher{" + "name=" + name + ", cls=" + cls + ", students=" + students + '}';
+        return "Teacher{" + "name=" + name + ", cls=" + cls + ", students=" + students + ", subject=" + subject + '}';
     }
+
+
 
 
 
