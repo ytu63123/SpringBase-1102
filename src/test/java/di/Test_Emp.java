1d80b72ce47e3dbbe38f100d4becb8a868bbe231
student <student@192.168.1.92>
2020-11-09 20:12:44 +0800

    SSH

--- src/test/java/di/Test_Emp.java
+++ src/test/java/di/Test_Emp.java
@@ -5,12 +5,16 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test_Emp {
+
     @Test
-    public void  t1(){
-   ClassPathXmlApplicationContext ctx= new ClassPathXmlApplicationContext("beans-config-2.xml");
-    Emp emp =ctx.getBean("emp",Emp.class);
+    public void t1() {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config-2.xml");
+        Emp emp = ctx.getBean("emp", Emp.class);
         System.out.println(emp);
-        
+        Emp emp2 = ctx.getBean("emp2", Emp.class);
+        System.out.println(emp2);
+         Emp emp3 =ctx.getBean("emp3",Emp.class);
+        System.out.println(emp3);
         ctx.close();
     }
 }
