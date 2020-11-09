d69b2c3ca9624cdc071fa0ef4ffb8bb19aba7262
student <student@192.168.1.92>
2020-11-09 19:17:34 +0800

    SSH

---
+++ src/test/java/di/Test_Programmer.java
@@ -0,0 +1,19 @@
+package di;
+
+import com.mycompany.springbasic1102.di.byname.MicrosoftProgrammer;
+import com.mycompany.springbasic1102.di.byname.OracleProgrammer;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test_Programmer {
+    @Test
+    public void  t1(){
+   ClassPathXmlApplicationContext ctx= new ClassPathXmlApplicationContext("beans-config-2.xml");
+        OracleProgrammer op=ctx.getBean("oracleProgrammer",OracleProgrammer.class);
+        System.out.println(op);
+        MicrosoftProgrammer mp=ctx.getBean("microsoftProgrammer",MicrosoftProgrammer.class);
+        System.out.println(mp);
+        
+        ctx.close();
+    }
+}
