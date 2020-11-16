62580ab85b093e5fbc70deee6acdb9ac8105a79d
student <student@192.168.1.92>
2020-11-16 21:10:29 +0800

    SSH

--- src/test/java/jdbc/Test1.java
+++ src/test/java/jdbc/Test1.java
@@ -1,8 +1,13 @@
 package jdbc;
 
+import com.mycompany.springbasic1102.jdbc.Emp;
+import java.util.List;
+import java.util.Map;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class Test1 {
 
@@ -15,7 +20,11 @@ public class Test1 {
         //create();
         //create("DeleteMe", 19, "女");
         // updateAgeById(1, 20);
-       // deleteById(3);
+        // deleteById(3);
+        // readAll();
+        //readAvgOfAge();
+        //readAllfroEntity();
+        readEntityById(2);
     }
 
     // CRUD - Create 新增資料
@@ -35,10 +44,48 @@ public class Test1 {
         String sql = "update emp set age=? where eid=?";
         jdbcTemplate.update(sql, age, eid);
     }
+
     // CRUD = Delete 刪除資料
     private void deleteById(Integer eid) {
         String sql = "Delete From Emp where eid=?";
         jdbcTemplate.update(sql, eid);
+    }
+
+    // CRUD - Read 查詢
+    private void readAll() {
+        String sql = "select *from emp";
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
+        System.out.println(list);
+        int sum = list.stream().mapToInt(m -> Integer.parseInt(m.get("age") + "")).sum();
+        System.out.println(sum);
+        double avg = list.stream().mapToInt(m -> Integer.parseInt(m.get("age") + "")).average().getAsDouble();
+        System.out.println(avg);
+    }
+
+    // CRUD - Read 查詢2
+    private void readAllfroEntity() {
+        String sql = "select *from emp";
+        // RowMapper
+        RowMapper<Emp> rm = new BeanPropertyRowMapper(Emp.class);
+        List<Emp> emps = jdbcTemplate.query(sql, rm);
+        System.out.println(emps);
+    }
+    // CRUD - Read 查詢單筆
+
+    private void readEntityById(Integer eid) {
+        String sql = "select *from emp where eid=?";
+        RowMapper<Emp> rm = new BeanPropertyRowMapper(Emp.class);
+        //Emp emp = jdbcTemplate.queryForObject(sql, rm, eid);
+        Emp emp = jdbcTemplate.queryForObject(sql, new Object[]{eid}, rm);
+        System.out.println(emp);
+
+    }
+
+    // CRUD - Read 查詢平均年齡
+    private void readAvgOfAge() {
+        String sql = "SELECT avg(cast(age as double)) FROM emp ";
+        double avg = jdbcTemplate.queryForObject(sql, Double.class);
+        System.out.println("avg=" + avg);
     }
 
 }
